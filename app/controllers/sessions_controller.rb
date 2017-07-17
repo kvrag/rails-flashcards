925243c0994ac369b6155807d5ed0aba0302e4dc
Chris McMenomy & Kristina Vragovic <pair+caristopmer.kvrag@devbootcamp.com>
2017-07-17 11:59:58 -0500

    logging in working.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+
+  end
+
+  def create
+    @user = User.find_by(email: params[:email])
+    if @user && @user.authenticate(params[:password])
+      session[:current_user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      @errors = ["Your shit ain't right"]
+      render "new"
+    end
+  end
+
+  def destroy
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   get '/' => 'pages#index'
+  get '/sessions/new' => 'sessions#new', as: "new_session"
+  post '/sessions' => 'sessions#create'
 
   resources :users
 end

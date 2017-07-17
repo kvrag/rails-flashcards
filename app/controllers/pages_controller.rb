2285d8d1ad5ba47ae36541dd997ce8911d66de51
Chris McMenomy & Kristina Vragovic <pair+caristopmer.kvrag@devbootcamp.com>
2017-07-17 14:18:20 -0500

    logging out working.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,8 @@ class PagesController < ApplicationController
 
   def index
   end
+
+  def session_helper
+    @uniqueword22 = session.inspect
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get '/' => 'pages#index'
   get '/sessions/new' => 'sessions#new', as: "new_session"
   post '/sessions' => 'sessions#create'
+  delete '/sessions' => 'sessions#destroy'
+  get '/session_helper' => 'pages#session_helper'
 
   resources :users
 end
